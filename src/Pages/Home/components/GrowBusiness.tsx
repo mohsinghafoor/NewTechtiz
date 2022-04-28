81a717809af11c25daf4000e709ea52ae3dbe7b0
mohsinghafoor <ch.mohsin.ghafoor@gmail.com>
2022-04-28 12:07:46 +0500

    grow business

--- src/Pages/Home/components/GrowBusiness.tsx
+++ src/Pages/Home/components/GrowBusiness.tsx
@@ -11,35 +11,51 @@ import {
   Image,
   List,
   ListItem,
+  Text,
   VStack,
 } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { images } from '../../../assets/images'
 import Colors from '../../../components/Constants/Colors'
-import { GrowBusinessData } from './PageData'
+import { GrowBusinessData, GrowBusinessImages } from './PageData'
 
 const GrowBusiness = () => {
+  const [selectedImage, setSelectedImage] = useState(0)
   return (
-    <VStack>
+    <VStack w='full' alignItems='center' justifyContent='center'>
       <Heading
         color={Colors.bluePrimary}
         fontWeight={700}
         fontSize='40px'
         lineHeight='64px'
-        marginTop='50px'
+        marginTop='100px'
       >
         How TechTiz Helps Your Business Grow
       </Heading>
 
-      <HStack w='full'>
-        <Box w='54%'>
-          <Accordion allowMultiple>
+      <HStack
+        w='full'
+        padding='20px'
+        justifyContent='center'
+        mt='50px !important'
+      >
+        <Box w='50%'>
+          <Accordion allowMultiple w='100%'>
             {GrowBusinessData.map((item, index) => (
-              <AccordionItem key={index}>
+              <AccordionItem
+                key={index}
+                onClick={() => setSelectedImage(index)}
+              >
                 <h2>
                   <AccordionButton>
                     <Box flex='1' textAlign='left'>
-                      {item.heading}
+                      <Text
+                        color={Colors.blueSecondary}
+                        fontSize='24px'
+                        lineHeight='63.98px'
+                      >
+                        {item.heading}
+                      </Text>
                     </Box>
                     <AccordionIcon
                       color={Colors.blueSecondary}
@@ -50,9 +66,17 @@ const GrowBusiness = () => {
                 </h2>
                 {item.list.map((listItem, index) => (
                   <AccordionPanel pb={4} key={index}>
-                    <List>
-                      <Image src={images.listBullet} />
-                      <ListItem>{listItem}</ListItem>
+                    <List display='flex' alignItems='flex-start'>
+                      <Image
+                        src={images.listBullet}
+                        marginTop='6px'
+                        marginRight='20px'
+                      />
+                      <ListItem>
+                        <Text fontSize='16px' lineHeight='30px'>
+                          {listItem}
+                        </Text>
+                      </ListItem>
                     </List>
                   </AccordionPanel>
                 ))}
@@ -61,8 +85,23 @@ const GrowBusiness = () => {
           </Accordion>
         </Box>
 
-        <Box w='46%'>
-          <Image src='https://techtiz.co/wp-content/uploads/2022/02/Group-2.png' />
+        <Box
+          // w='46%'
+
+          paddingLeft='80px'
+        >
+          {GrowBusinessImages.map(
+            (image, index) =>
+              index === selectedImage && (
+                <Image
+                  src={image}
+                  alt='image'
+                  key={index}
+                  height='350px'
+                  width='400px'
+                />
+              )
+          )}
         </Box>
       </HStack>
     </VStack>
